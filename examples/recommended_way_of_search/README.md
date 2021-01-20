e1ef7673bb1a6cd9a8a88f130fa56a0ca1e22a9b
Shandorius <44729715+Shandorius@users.noreply.github.com>
2021-01-20 12:04:49 +0100

    Added example for my recommended search method
    Added example for my recommended search method after my fork adding the ability to fetch  MeSH terms

---
+++ examples/recommended_way_of_search/README.md
@@ -0,0 +1,6 @@
+# Example - Recommended Way of Search
+
+This example shows how to perform a search on the PubMed database and retrieve information about the relevant articles.
+This explanation has been added since I forked the pymed library, it is the way I (@Shandorius) recommend running the searches now.
+
+You can use [PubMed query builder](https://www.ncbi.nlm.nih.gov/pubmed/advanced) for creating the query syntax.
---
+++ examples/recommended_way_of_search/main.py
@@ -0,0 +1,52 @@
+from pymed import PubMed
+import pandas as pd
+import datetime
+import time
+import json
+
+pubmed = PubMed(tool="toolname", email="your email")
+
+# Timer
+start_time = time.time()
+
+industryList = []  # Array of companys
+
+for comp in industryList:
+    comp_time = time.time()
+    articleList = []
+    articleInfo = []
+    query = f"your test query here with {comp}, same as how pubmed takes the queries"
+    results = pubmed.query(query, max_results=999999)
+    for article in results:
+        articleDict = article.toDict()
+        articleList.append(articleDict)
+    for article in articleList:
+        pubmedId = article['pubmed_id'].partition('\n')[0]
+        articleInfo.append({'pubmed_id': pubmedId,
+                            'title': article['title'],
+                            'keywords': article['keywords'],
+                            'mesh': article['mesh'],
+                            'journal': article['journal'],
+                            'abstract': article['abstract'],
+                            'conclusions': article['conclusions'],
+                            'methods': article['methods'],
+                            'results': article['results'],
+                            'copyrights': article['copyrights'],
+                            'doi': article['doi'],
+                            'publication_date': str(article['publication_date']),
+                            'authors': article['authors']})
+
+    # Export to both CSV and .txt files
+    articlesPD = pd.DataFrame.from_dict(articleInfo)
+    export_csv = articlesPD.to_csv(
+        f'Pubmed_DataFrame_{comp}.csv', index=None, header=True)
+    with open(f"PubMed_JSON_{comp}.txt", "w") as outfile:
+        json.dump(articleInfo, outfile)
+
+    # Print time it took program to run said company files
+    print(f"{comp} DataFrame Done in {(time.time() - comp_time)} seconds OR {((time.time() - comp_time) / 60)} minutes")
+
+# Print time it took to run script
+print(
+    f"Full execution took {(time.time() - start_time)} seconds OR {((time.time() - comp_time) / 60)} minutes")
+print("done")
